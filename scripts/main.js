103ba6658527c0be91a51b8ecbcf73a9892efaaa
Shadik372 <rageshadik96@gmail.com>
2026-03-07 13:07:17 +0600

    Added Functionality for Issue Counter

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 console.log("connected");
+
+const loadIssueCounterAPI = ()=>{
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then(res=>res.json())
+    .then(Element=>{
+        const issues = Element.data;
+        const issuesCounter = issues.length;
+        
+        const counterContainer = document.getElementById("issues-counter")
+        counterContainer.innerText = issuesCounter;
+    })
+}
+loadIssueCounterAPI();
